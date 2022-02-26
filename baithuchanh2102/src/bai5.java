5c133280ed1ef0764ef5adece605acbc04beb17b
haduytb <99583372+haduytb@users.noreply.github.com>
2022-02-26 14:52:45 +0700

    baithuchanh2102

---
+++ baithuchanh2102/src/bai5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class bai5 {
+
+   public static void main(String[] args) throws Exception {
+    Scanner sc =new Scanner(System.in);
+    int n =0;
+    for (; n < 5;) {
+        System.out.println(n);
+        n++;
+        } 
+    }
+}
